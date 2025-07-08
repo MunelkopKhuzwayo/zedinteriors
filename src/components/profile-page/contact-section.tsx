53e0de7b928a0213e134a6e98bb7adef533795e2
munelkop.khuzwayo <munelkop.khuzwayo@gmail.com>
2025-07-08 07:42:31 +0000

    contact section update

--- src/components/profile-page/contact-section.tsx
+++ src/components/profile-page/contact-section.tsx
@@ -11,48 +11,48 @@ export function ContactSection() {
         <p className="mt-4 max-w-xl mx-auto text-lg text-muted-foreground">
           I'm currently available for work. If you want to turn you property into a beautiful functional space, let's talk.
         </p>
-        <div className="mt-8 flex justify-center gap-4">
-          <Button
-            asChild
-            variant="outline"
-            size="icon"
-            className="h-14 w-14 rounded-full bg-background transition-transform hover:scale-110"
-          >
-            <a href="mailto:alex.doe@example.com" aria-label="Email">
-              <Mail className="h-6 w-6" />
-            </a>
-          </Button>
-          <Button
-            asChild
-            variant="outline"
-            size="icon"
-            className="h-14 w-14 rounded-full bg-background transition-transform hover:scale-110"
-          >
-            <a href="#" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
-              <UserCircle className="h-6 w-6" />
-            </a>
-          </Button>
-          <Button
-            asChild
-            variant="outline"
-            size="icon"
-            className="h-14 w-14 rounded-full bg-background transition-transform hover:scale-110"
-          >
-            <a href="#" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
-              <Image className="h-6 w-6" />
-            </a>
-          </Button>
-          <Button
-            asChild
-            variant="outline"
-            size="icon"
-            className="h-14 w-14 rounded-full bg-background transition-transform hover:scale-110"
-          >
-            <a href="#" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
-              <PhoneCall className="h-6 w-6" />
-            </a>
-          </Button>
+        <div className="mt-8 flex flex-row justify-center gap-6">
+          <div className="">
+            <Button
+              asChild
+              variant="outline"
+              size="icon"
+              className="h-14 w-14 rounded-full bg-background transition-transform hover:scale-110"
+            >
+              <a href="mailto:sedadakun900@gmail.com" aria-label="Email">
+                <Mail className="h-6 w-6" />
+              </a>
+            </Button>
+            <h3 className="">Email</h3>
+          </div>
+          <div>
+            <Button
+              asChild
+              variant="outline"
+              size="icon"
+              className="h-14 w-14 rounded-full bg-background transition-transform hover:scale-110"
+            >
+              <a href="https://www.instagram.com/zed.adakun" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
+                <Image className="h-6 w-6" />
+              </a>
+            </Button>
+            <h3 className="">Instagram</h3>
+          </div>
+          <div>
+            <Button
+              asChild
+              variant="outline"
+              size="icon"
+              className="h-14 w-14 rounded-full bg-background transition-transform hover:scale-110"
+            >
+              <a href="tel: +27630576959" aria-label="phone" target="_blank" rel="noopener noreferrer">
+                <PhoneCall className="h-6 w-6" />
+              </a>
+            </Button>
+            <h3 className="">WhatsApp</h3>
+          </div>
         </div>
+        
       </div>
     </section>
   );
